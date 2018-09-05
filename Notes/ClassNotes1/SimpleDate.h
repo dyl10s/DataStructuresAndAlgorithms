e66c1f84878f10f63934c2843f210e684e999c57
dyl10s <dylanstrohschein@gmail.com>
2018-09-05 17:35:21 -0400

    Made Notes Folder

---
+++ Notes/ClassNotes1/SimpleDate.h
@@ -0,0 +1,19 @@
+//
+// Created by dylan on 9/5/18.
+//
+
+#ifndef CLASSNOTES1_SIMPLEDATE_H
+#define CLASSNOTES1_SIMPLEDATE_H
+
+
+class SimpleDate {
+
+public:
+    SimpleDate(int day, int month, int year);
+
+    //This is a Destructor
+    ~SimpleDate();
+};
+
+
+#endif //CLASSNOTES1_SIMPLEDATE_H
---
+++ Notes/ClassNotes1/cmake-build-debug/CMakeFiles/ClassNotes1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassNotes1.dir/main.cpp.o"
+  "CMakeFiles/ClassNotes1.dir/SimpleDate.cpp.o"
+  "ClassNotes1.pdb"
+  "ClassNotes1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassNotes1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
