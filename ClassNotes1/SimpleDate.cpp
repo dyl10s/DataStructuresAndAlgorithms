ebbb343e5836b7f5c2716966c081ffe80f1e0c72
dyl10s <dylanstrohschein@gmail.com>
2018-09-05 16:39:10 -0400

    Class notes

---
+++ ClassNotes1/SimpleDate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dylan on 9/5/18.
+//
+
+#include "SimpleDate.h"
---
+++ ClassNotes1/SimpleDate.h
@@ -0,0 +1,14 @@
+//
+// Created by dylan on 9/5/18.
+//
+
+#ifndef CLASSNOTES1_SIMPLEDATE_H
+#define CLASSNOTES1_SIMPLEDATE_H
+
+
+class SimpleDate {
+
+};
+
+
+#endif //CLASSNOTES1_SIMPLEDATE_H
