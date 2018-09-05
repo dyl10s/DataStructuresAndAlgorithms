a968f58615205e1e9a83c02a78080cd5447fffe9
dyl10s <dylanstrohschein@gmail.com>
2018-09-05 17:32:32 -0400

    Edited Main

--- HW1-Date/hw01-date.cpp
+++ HW1-Date/hw01-date.cpp
@@ -2,7 +2,24 @@
 #include "SimpleDate.h"
 
 int main() {
-    SimpleDate *date = new SimpleDate(5, 8, 2011);
-    std::cout << date->nextDate().getMonth() << "\\" << date->nextDate().getDay() << "\\" << date->nextDate().getYear();
+
+    SimpleDate date{5, 10, 2017};
+
+    std::cout << date.getDay();
+    std::cout << date.getMonth();
+    std::cout << date.getYear();
+    std::cout << date.isLeapYear();
+    std::cout << date.isLeapYear(2018);
+    std::cout << date.daysInMonth(6, 2012);
+    std::cout << date.isValidDate(5, 16, 1999);
+    std::cout << date.ordinalDate();
+    std::cout << date.daysInYear(2011);
+    std::cout << date.dayOfWeek();
+
+    SimpleDate other{1, 2, 1999};
+    std::cout << date.compareTo(other);
+
+    date.nextDate();
+    date.daysFromNow(10);
     return 0;
 }
