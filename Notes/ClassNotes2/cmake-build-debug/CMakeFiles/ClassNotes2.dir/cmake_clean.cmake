8fc105cd8a921734bb335ca3179aa0dfbc42a15c
dyl10s <dylanstrohschein@gmail.com>
2018-09-10 15:32:48 -0400

    Class notes

---
+++ Notes/ClassNotes2/cmake-build-debug/CMakeFiles/ClassNotes2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassNotes2.dir/main.cpp.o"
+  "ClassNotes2.pdb"
+  "ClassNotes2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassNotes2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
