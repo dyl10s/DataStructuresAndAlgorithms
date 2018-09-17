0286e3df056e679d605e0d2a2a997d4e37d223f1
dyl10s <dylanstrohschein@gmail.com>
2018-09-17 15:19:35 -0400

    HW02 Max Value

---
+++ Homework/hw02-maxvalue/cmake-build-debug/CMakeFiles/hw02_maxvalue.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw02_maxvalue.dir/main.cpp.o"
+  "hw02_maxvalue.pdb"
+  "hw02_maxvalue"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw02_maxvalue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Homework/hw02-maxvalue/cmake-build-debug/CMakeFiles/hw02_maxvalue.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hw02_maxvalue.
+# This may be replaced when dependencies are built.
